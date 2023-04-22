68dddbfaa2402a894a16a0af18bb6459a516bcab
dioliviers <dioliviers@gmail.com>
2023-04-21 23:58:29 -0300

    Sensibilidade de case no php (case sensitive)

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-    echo "Diogo";
+// Não é case sensitive
+    echo "Diogo <br>";
+    echo "Oliveira <br>";
+    echo "Santos <br>";
+
+// É case sensitive
+
+$nome= "Diogoh";
+$NOME= "teste";
+echo $nome;
+echo "<br>";
+echo $NOME;
 ?>
