9059b1e1e73d36f563552767c23d219ec6107b94
dioliviers <dioliviers@gmail.com>
2023-04-28 16:45:25 -0300

    checando se um dado é boolean

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
 <?php
-    echo true; /* verdadeiro */
+   $a = true;
 
-    if (true){ /* aqui colocamos o bollean direto na expressão */
-        echo "verdadeiro ! <br>"; 
+    if(is_bool($a)){
+        echo "é um booleano ! <br>";
     }
-
-    if (5 > 2){ /* retorna true (verdadeiro ou false caso não seja com a logica da expressão) */
-        echo "é verdadeiro <br>";
+    if(is_bool(0)){ /* 0 é um número (false) não exibe nada */
+        echo "é um booleano !";
     }
-
-    $pode_entrar = true; /* Pode entrar é true */
-        if($pode_entrar){ /* se pode entrar == true ele exibe o conteúdo do bloco if. */
-            echo "Pode entrar pau nu cu";
-        }
+    if(is_bool(false)){ /* aqui ele vai exibir o valor de false  */
+        echo "é um booleano false!";
+    }
 ?>
