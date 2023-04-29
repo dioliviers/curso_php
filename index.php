4581cbb29cfdcb48e80d49bdf92ebf8a4e44c075
dioliviers <dioliviers@gmail.com>
2023-04-28 21:49:11 -0300

    trabalhando com array com chave e valor e exibindo com a ferramenta echo e print_r com php

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
 <?php
-/* print_r ele mostra o indice do array, echo mostra o conteúdo do indice se inserirmos somente a variavel
-ele exibe o array com todos os conteúdos que existe no array, se inserirmos a variavel e o indice entre 
-colchetes exibimos o indice "print_r" ou o conteúdo do array "echo*/
+    $arr = [ 'nome' => 'thor', 'patas' => 4, 'cor' => 'Marrom' ]; /* inserido no array CHAVE e VALOR, quando
+    for chamado a chave na variável do array ele exibira o valor atribuido a chave. */
 
-   $a = [1,2,3]; /* Armazena um array de numeros inteiros */
-    print_r($a);/* exibe o indice e o conteúdo do Array */
+    echo $arr["nome"];/* exibe o valor da chave nome */
     echo "<br>";
-    echo $a[1];/* exibe o indice 1 da variavel a */
-    $arr = ['Diogo', 29, true];/*  armazena uma string, um inteiro e um boleano true */
+    echo $arr["patas"];/* exibe o valor da chave pata */
     echo "<br>";
-    print_r($arr); /* exibe o indice e o conteúdo do array */
-    echo "<br>";
-    print_r($arr[1]); /* exibe o indice do array mas não exibe o conteúdo */
+    print_r($arr);/* com o print_r ele exibe a chave e o valor atribuido na variavel */
+
+
 ?>
