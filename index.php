dfc2c71ce3beca4adfc9e566df8aef95c35cbf89
dioliviers <dioliviers@gmail.com>
2023-04-28 21:23:04 -0300

    Trabalhando com array utilizando echo e print_r em php

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
-   $a = true;
+/* print_r ele mostra o indice do array, echo mostra o conteúdo do indice se inserirmos somente a variavel
+ele exibe o array com todos os conteúdos que existe no array, se inserirmos a variavel e o indice entre 
+colchetes exibimos o indice "print_r" ou o conteúdo do array "echo*/
 
-    if(is_bool($a)){
-        echo "é um booleano ! <br>";
-    }
-    if(is_bool(0)){ /* 0 é um número (false) não exibe nada */
-        echo "é um booleano !";
-    }
-    if(is_bool(false)){ /* aqui ele vai exibir o valor de false  */
-        echo "é um booleano false!";
-    }
+   $a = [1,2,3]; /* Armazena um array de numeros inteiros */
+    print_r($a);/* exibe o indice e o conteúdo do Array */
+    echo "<br>";
+    echo $a[1];/* exibe o indice 1 da variavel a */
+    $arr = ['Diogo', 29, true];/*  armazena uma string, um inteiro e um boleano true */
+    echo "<br>";
+    print_r($arr); /* exibe o indice e o conteúdo do array */
+    echo "<br>";
+    print_r($arr[1]); /* exibe o indice do array mas não exibe o conteúdo */
 ?>
