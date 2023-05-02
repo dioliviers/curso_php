3eef521d16f1ceda490e49a703ec7e89fd327522
dioliviers <dioliviers@gmail.com>
2023-05-02 02:13:52 -0300

    alterando valores de variáveis

--- index.php
+++ index.php
@@ -1,17 +1,10 @@
 <?php
-/* EXERCICIO 
-Crie duas variáveis com números
-Crie uma terceira veriavel com a soma desses dos números 
-exiba*/
-  $num1 = 10;
-  $num2 = 20;
-  $soma = $num1 + $num2;
-  echo $soma;
-
-  function somar ($soma){  /* utilizando os resultados em uma função */
-    echo "O resultado da soma é $soma";
-  }
-  echo "<br>";
-  echo somar($soma); /* retornando o valor de soma ao chamar a função somar */
+/* trocando valores de valiável */
+    $x = 10;
+    echo $x;
+    echo "<br>";   
+    $x = "oi";
+    echo "<br>";
+    echo $x;
   
 ?>
