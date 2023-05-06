c7de41e8b98ab94046a6b15e3a580ada3039484f
dioliviers <dioliviers@gmail.com>
2023-05-06 16:32:38 -0300

    Trabalhando com concatenacao

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
-    /* Exercicio 13
-    Teste o operador de resto em duas divisões,
-    Uma exata e outra não exata */
+    /* concatenação */
+    
+    echo "Testando a concatenação";
+    echo "<br>";
+    echo "testando" . " a " . "concatenação";
+    echo "<br>";
 
-    $div1 = 5 % 5;
-    $div2 = 5 % 4; 
-
-    echo "O resultado da divisão exata é: $div1" . "<br>";
-    echo "O Resultado da divisão não exata é: $div2" . "<br>";
-
+    $t = "testando ";
+    echo "<br>";
+    $c = "Concatenação"; 
+    
+    echo $t . " a " . $c;
 
 ?>
