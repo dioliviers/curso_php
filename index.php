db60394a5ec113e540891e822d78b241b4c0b289
dioliviers <dioliviers@gmail.com>
2023-05-06 16:20:07 -0300

    Utilizando operador % e exibindo o resto da divisão exata e não exata

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
 <?php
-/* Exercicio 12 
-    Crie um arquivo php;
-    Crie uma operação com cada um dos operadores básicos;
-    Cada operação deve estar em uma vairável;
-    imprima cada uma das etapas;
-*/
-    $soma = 10 + 10;
-    $subtracao = 20 + 5;
-    $divisao = 10 / 10;
-    $multiplicacao = 5 * 5 ;
+    /* Exercicio 13
+    Teste o operador de resto em duas divisões,
+    Uma exata e outra não exata */
 
-    echo " O resultado da soma é: $soma" . "<br>";
-    echo "O resultado da subtracao é: $subtracao" . "<br>";
-    echo "O resultado da divisao é: $divisao" . "<br>";
-    echo "O Resultado da multiplicação é: $multiplicacao" . "<br>";
+    $div1 = 5 % 5;
+    $div2 = 5 % 4; 
+
+    echo "O resultado da divisão exata é: $div1" . "<br>";
+    echo "O Resultado da divisão não exata é: $div2" . "<br>";
+
+
 ?>
