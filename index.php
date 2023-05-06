523142c6160ff81f6ecae678636d988ab5d88da0
dioliviers <dioliviers@gmail.com>
2023-05-06 16:04:32 -0300

    exercicio 12 - trabalhando com operadores aritmeticos

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php
-/* Ordem dos operadores aritmeticos*/
-    $soma = 5+5;
-    echo $soma . "<br>";
-    
-    $subtracao = 10-2;
-    echo $subtracao . "</br>";
-    
-    $multi = 2*199;
-    echo $multi . "</br>";
+/* Exercicio 12 
+    Crie um arquivo php;
+    Crie uma operação com cada um dos operadores básicos;
+    Cada operação deve estar em uma vairável;
+    imprima cada uma das etapas;
+*/
+    $soma = 10 + 10;
+    $subtracao = 20 + 5;
+    $divisao = 10 / 10;
+    $multiplicacao = 5 * 5 ;
 
-    $divisao = 123/8;
-    echo $divisao . "</br>";
+    echo " O resultado da soma é: $soma" . "<br>";
+    echo "O resultado da subtracao é: $subtracao" . "<br>";
+    echo "O resultado da divisao é: $divisao" . "<br>";
+    echo "O Resultado da multiplicação é: $multiplicacao" . "<br>";
 ?>
