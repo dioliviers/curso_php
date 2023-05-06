8544fd72ccdfaf8e103bc1cf50971595a6b74e8b
dioliviers <dioliviers@gmail.com>
2023-05-05 22:09:01 -0300

    Ordem dos operadores

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
-/* ESCOPO (Parâmetros de função) */
+/* Ordem dos operadores*/
 
-    function soma( $a, $b ){ /* criamos variáveis como parametros */
-        echo $a + $b; /* utilizamos os valores da variável para realizar uma soma */
-    }; 
- 
-    soma( 2, 4); /* aqui chama */
-    soma( 6, 8);
-    soma( 10,10);
+    echo (3+5) *2;
+    echo 3/2;
+    echo 3-5;
+
+    $a = 5;
+    $b = 2;
+    $c = 10;
+    
+    echo $a + $b / $c;
+    echo "<br>";
+    echo $c +$b / $a;
+
+    
 ?>
