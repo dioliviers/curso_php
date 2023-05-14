539c14e69265d237f080febf9612c38fdc63ae43
dioliviers <dioliviers@gmail.com>
2023-05-14 19:50:21 -0300

    exercicio 19 trabalhando com CAST ( Conversão ) na pratica

--- index.php
+++ index.php
@@ -1,25 +1,16 @@
 <?php
-    /*  Operadores de expressão */
-    $a =  (int) "12";
-    echo $a;
+    /*  Exercicio 19 
+    converta os dados para INT com o operador CAST (Conversão) */
+
+    $a = (int) "testando";
+    $b = (int) 12.9;
+    $c = (int) true;
+    $d = (int) [1,2,3];
+
     echo "<br>";
-
-    echo $a + 12 ;
-
-    if ( $a === 12 ) {
-        echo " <br> A é idêntico a 12 ";
-    } 
-
-    $b = (float) "3.53515";
-
-    if ( $b === 3.53515 ) {
-        echo " <br> B é idêntico a 3.35 ";
-    } 
-
-    $c = (string) 34;
-    echo "<br>";
+    echo "$a . <br>"; /* aqui ele vai retornar 0 porque não da pra converter string para número */
+    echo "$b . <br>"; /*  aqui ele retorna o número inteiro 12 */
+    echo "$c . <br>";/*  aqui ele retorna 1 porque true é um boleano verdadeiro */
+    echo "$d . <br>"; /*  aqui ele retorna 1 porque é um array de números */
     
-    if ( $c === "34" ) {
-        echo "O operador C é idêntico a 34";
-    }
 ?>
