5999b7ace85414194832bb14b40ce7b6f1ff1534
dioliviers <dioliviers@gmail.com>
2023-05-14 22:28:47 -0300

    Operadores ternários

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 <?php
-    /*  Exercicio 19 
-    converta os dados para INT com o operador CAST (Conversão) */
+    /* Operadores ternarios */
 
-    $a = (int) "testando";
-    $b = (int) 12.9;
-    $c = (int) true;
-    $d = (int) [1,2,3];
+    //true
+    echo 20 > 10 ? "Olá mundo" : "Deu false" ;
+
+    //false
 
     echo "<br>";
-    echo "$a . <br>"; /* aqui ele vai retornar 0 porque não da pra converter string para número */
-    echo "$b . <br>"; /*  aqui ele retorna o número inteiro 12 */
-    echo "$c . <br>";/*  aqui ele retorna 1 porque true é um boleano verdadeiro */
-    echo "$d . <br>"; /*  aqui ele retorna 1 porque é um array de números */
-    
+    echo 20 > 50 ? "Olá mundo" : "Deu false" ;
 ?>
