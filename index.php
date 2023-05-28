d8eb428fba5691d75c38a9adb8ff70b680656db1
dioliviers <dioliviers@gmail.com>
2023-05-28 20:26:38 -0300

    faça uma veriificação de idade se for maior TRUE se for menor FALSE

--- index.php
+++ index.php
@@ -1,24 +1,30 @@
 <?php
-    /* Exercicio 21
-     Faça as seguintes verificações
-     - 5 é maior que 2?
-     - Matheus é diferente de Pedro
-     - 12 é menor ou igual a 11 ?
-     - Você deve inserir valores em variáveis
+
+    /* Exercicio 22
+    - Crie variáveis que recebem idades;
+    - Cheque se as idades são maiores ou iguais a 18,
+    caso seja TRUE imprima uma mensagem que a pessoa é maior de idade.
      */
+     $validador = '';
+     $idade = $validador;
+     $idade = 'oi';
 
-    $idade = 5;
-    $nome = 'Matheus';
-    $resultado =[];
-    $resultado2 = [];
-
-    $resultado = $idade > 4 ? "$idade é maior que 2 " : " $idade inserido não é maior ";
-    echo $resultado . "<br>";
-
-    $resultado2 = $nome != 'Pedro' ? " $nome é diferente " : " São iguais ";
-    echo $resultado2 . "<br>";
-
-    echo 12 >= 10 ? "  é menor " : " é maior <br>";
-
-
+     $idade2 = $validador;
+     $idade2 = 20;
+   
+        function verificaIdade($validador){
+            if(empty($validador)){
+                echo "é necessário preencher um valor para validação <br>";
+            }
+            elseif( !is_numeric($validador)){
+                echo "insira apenas números <br> ";
+            }
+            elseif($validador >= 18){
+                echo "Pode entrar maior de idade <br> ";
+            }elseif ($validador < 18 ) {
+                echo " Menor de idade <br> ";
+            }
+        }
+    verificaIdade($idade);
+    verificaIdade($idade2);
 ?>
