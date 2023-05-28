bbeefa29b5d9993278604d6b3ecb716a78ff3f77
dioliviers <dioliviers@gmail.com>
2023-05-28 17:34:56 -0300

    exercicio 21 utilizando ternários

--- index.php
+++ index.php
@@ -1,13 +1,24 @@
 <?php
-    /* Exercicio 20
-     Faça uma condicional utilizando operadores ternarios 
+    /* Exercicio 21
+     Faça as seguintes verificações
+     - 5 é maior que 2?
+     - Matheus é diferente de Pedro
+     - 12 é menor ou igual a 11 ?
+     - Você deve inserir valores em variáveis
      */
 
-     $a = 10;
-     $b = 5;
+    $idade = 5;
+    $nome = 'Matheus';
+    $resultado =[];
+    $resultado2 = [];
 
-     echo $a <= $b ? "A é menor ou igual a B <br>" : "B é menor que A <br> ";
+    $resultado = $idade > 4 ? "$idade é maior que 2 " : " $idade inserido não é maior ";
+    echo $resultado . "<br>";
 
-     echo $b >= $a ? " B é maior ou igual a A" : "B é menor ou igual a A";
+    $resultado2 = $nome != 'Pedro' ? " $nome é diferente " : " São iguais ";
+    echo $resultado2 . "<br>";
+
+    echo 12 >= 10 ? "  é menor " : " é maior <br>";
+
 
 ?>
