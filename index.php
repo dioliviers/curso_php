1587c2ce999b80dc181fa9e4786818e70396a3b4
dioliviers <dioliviers@gmail.com>
2023-06-08 17:58:52 -0300

    exercicio 26 praticando novamente exercicio com bloco de condições

--- index.php
+++ index.php
@@ -1,28 +1,20 @@
 <?php
-    /* Exercicio 25
-    Crie variáveis com números e outras com string 
-    Faça um if checando se é um número;
-    caso for, atribua a multiplicação deste número por 2 em outra variável;
-    Crie outro if, que checa se o novo número é maior que 100;
-    Se sim, imprima uma mensagem*/
+    /* Crie uma variavel que recebe uma velocidade de um carro;
+    Depois crie uma estrutura IF que verifica essa velocidade
+    se a velocidade for menor que 40 , imprima que o motorista está na velocidade correta
+    Se igual a 40 imprima uma mensagem para o motorista tomar cuidado.;
+    se for maior de 40, imprima uma mensagem de multa; */
 
-    $num1 = 101;
-    $num2 = 40;
-    $teste = 'teste';
-    if ( is_int($num1) || is_float($num1) ){
-        $mult = $num1 * 2;
-        if( $mult > 100 ) {
-            echo 'o número é maior que 100 <br> ';
-        }else { 
-            echo ' O número é menor que 100 <br> ';
-        }
-    };
-    if ( is_int($num2) || is_float($num2) ){
-        $mult = $num2 * 2;
-        if( $mult > 100 ) {
-            echo 'o número é maior que 100 <br> ';
-        }else { 
-            echo ' O número é menor que 100 <br> ';
-        }
-    };
+    $velocidade_exigida = 40;
+    $velocidade_carro = 39;
+    if( $velocidade_carro < $velocidade_exigida ){
+        echo "Você está na velocidade correta";
+        
+    }elseif($velocidade_carro == $velocidade_exigida){
+        echo "Tome cuidado !";
+    }else{
+        echo "Você tomou uma multa !";
+    }
+
 ?>
+
