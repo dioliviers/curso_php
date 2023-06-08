e72d5f47f09767b5a940d453c088446a317aef2e
dioliviers <dioliviers@gmail.com>
2023-06-07 23:40:24 -0300

    exercicio 24 trabalhando com if ( usei ternario )

--- index.php
+++ index.php
@@ -1,30 +1,8 @@
 <?php
+/* Crie uma variável que recebe um peso,
+Caso seja maior que 80, imprima a mensagem q está pesado demais,
+senão, imprima " Peso dentro do limite */
 
-    /* Exercicio 22
-    - Crie variáveis que recebem idades;
-    - Cheque se as idades são maiores ou iguais a 18,
-    caso seja TRUE imprima uma mensagem que a pessoa é maior de idade.
-     */
-     $validador = '';
-     $idade = $validador;
-     $idade = 'oi';
-
-     $idade2 = $validador;
-     $idade2 = 20;
-   
-        function verificaIdade($validador){
-            if(empty($validador)){
-                echo "é necessário preencher um valor para validação <br>";
-            }
-            elseif( !is_numeric($validador)){
-                echo "insira apenas números <br> ";
-            }
-            elseif($validador >= 18){
-                echo "Pode entrar maior de idade <br> ";
-            }elseif ($validador < 18 ) {
-                echo " Menor de idade <br> ";
-            }
-        }
-    verificaIdade($idade);
-    verificaIdade($idade2);
+    $peso = 70;
+        echo  $peso > 80 ?  'Pesado demais' : 'Peso está dentro do limite';
 ?>
