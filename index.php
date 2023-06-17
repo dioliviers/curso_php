9f49bd022c4c47e310a3453bb8c3dacc866d9f3f
dioliviers <dioliviers@gmail.com>
2023-06-17 19:49:01 -0300

    faça uma comparação dos números recebidos , valide em uma função e retorne se o maior, menor e igual inserido pelo usuário

--- index.php
+++ index.php
@@ -1,19 +1,15 @@
 <?php
-    $nome = "Diogo";
-    switch($nome){
-        case "Pedro": /* verifica se a variável é == a String "Pedro" */
-            echo "Pedro não é igual ao nome mencionado";
-            break; /* Se for igual a Pedro a variavel nome o break para a leitura aqui senão ele continua as validações */
-        case "Lubia":
-            echo "Lubia não é igual ao nome mencionado";
-            break;
-        default:/* aqui é tipo um else, se ele validar todos os valores e nenhum for TRUE ele devolve  a informação abaixo */
-            echo "O nome não foi encontrado !";
-
+    
+    function compararNumeros($num1,$num2){
+        if($num1 > $num2){
+                return "O número $num1 é maior que $num2";
+            }elseif ($num2 > $num1) {
+                return " O número $num2 é maior que $num1";
+            }elseif ($num1 === $num2){
+                return "Os números $num1 e $num2 são iguais";
+        }
     }
-
-    /* Em resumo, se você tiver uma série de condições relacionadas que devem ser testadas em ordem, use "elseif".
-     Se tiver uma variável ou expressão com diferentes valores e precisar executar blocos de código diferentes com base nesses valores, use "switch case".
-    A escolha dependerá da lógica do seu programa e da forma como as condições ou valores estão estruturados. */
+    echo compararNumeros(5, 8);  // Output: O segundo número é maior.
+    echo compararNumeros(10, 5);  // Output: O primeiro número é maior.
+    echo compararNumeros(7, 7);  // Output: Os números são iguais.
 ?>
-
