321d42b5e2c4cd394b4f288bb472e919233315e9
dioliviers <dioliviers@gmail.com>
2023-06-17 19:34:57 -0300

    Exercicio utilizando switch case

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
 <?php
-    /* Crie uma variavel que recebe uma velocidade de um carro;
-    Depois crie uma estrutura IF que verifica essa velocidade
-    se a velocidade for menor que 40 , imprima que o motorista está na velocidade correta
-    Se igual a 40 imprima uma mensagem para o motorista tomar cuidado.;
-    se for maior de 40, imprima uma mensagem de multa; */
+    $nome = "Diogo";
+    switch($nome){
+        case "Pedro": /* verifica se a variável é == a String "Pedro" */
+            echo "Pedro não é igual ao nome mencionado";
+            break; /* Se for igual a Pedro a variavel nome o break para a leitura aqui senão ele continua as validações */
+        case "Lubia":
+            echo "Lubia não é igual ao nome mencionado";
+            break;
+        default:/* aqui é tipo um else, se ele validar todos os valores e nenhum for TRUE ele devolve  a informação abaixo */
+            echo "O nome não foi encontrado !";
 
-    $velocidade_exigida = 40;
-    $velocidade_carro = 39;
-    if( $velocidade_carro < $velocidade_exigida ){
-        echo "Você está na velocidade correta";
-        
-    }elseif($velocidade_carro == $velocidade_exigida){
-        echo "Tome cuidado !";
-    }else{
-        echo "Você tomou uma multa !";
     }
 
+    /* Em resumo, se você tiver uma série de condições relacionadas que devem ser testadas em ordem, use "elseif".
+     Se tiver uma variável ou expressão com diferentes valores e precisar executar blocos de código diferentes com base nesses valores, use "switch case".
+    A escolha dependerá da lógica do seu programa e da forma como as condições ou valores estão estruturados. */
 ?>
 
