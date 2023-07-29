2b49af97d20b56fa158df2b46a0b78bdf5b9fb75
dioliviers <dioliviers@gmail.com>
2023-07-29 16:29:16 -0300

    validação com while

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 <?php
-    
-    function compararNumeros($num1,$num2){
-        if($num1 > $num2){
-                return "O número $num1 é maior que $num2";
-            }elseif ($num2 > $num1) {
-                return " O número $num2 é maior que $num1";
-            }elseif ($num1 === $num2){
-                return "Os números $num1 e $num2 são iguais";
-        }
-    }
-    echo compararNumeros(5, 8);  // Output: O segundo número é maior.
-    echo compararNumeros(10, 5);  // Output: O primeiro número é maior.
-    echo compararNumeros(7, 7);  // Output: Os números são iguais.
+    $x= 0;
+    while($x < 10 ){
+       echo "0 X não é $x <br>";
+       $x++;
+       if($x === 5){
+        echo "Encontramos o valor";
+        break;
+       }
+    };
 ?>
