688f53fbae403e3d900499d94e64eb664cccb3c1
dioliviers <dioliviers@gmail.com>
2023-09-30 19:44:22 -0300

    percorrendo numeros inteiros e inserindo em uma variavel como array

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-    $arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+   $arr =[];
 
-    for ($i = 0; $i < count($arr); $i++) {
-        if ($arr[$i] % 2 == 0) {
-            echo "Numero: $arr[$i] <br>";
-        }
-    }
+   for ($i= 0; $i <= 10; $i++){
+    array_push($arr,$i); /* array_push, pega a variavel vazia e insere o valor do segundo paramentro na primeira variavel como array */
+   }
+   print_r($arr);/* exibe o array */
+  
 ?>
