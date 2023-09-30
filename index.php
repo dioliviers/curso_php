6b176a6239b0d2cc581c323e9e17e7350c4e0ad9
dioliviers <dioliviers@gmail.com>
2023-09-30 16:23:24 -0300

    exercicio 29 trabalhando com array , while, e loopings

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <?php
-    $i = 1;
-    while($i <= 10){
-       echo "O loop externo $i <br>";
-       
-       $j = 1;
-       while($j <= 5){
-        echo "looping interno $j <br>";
+    $arr = [10,20,30,40,50,60,70,80,90,100];
+    $i = 0;
 
-        $j++;
-       }
-
-
-       $i++;
+    while( $i < count($arr) ){
+        $numeroAtual = $arr[$i];
+        if( $numeroAtual== 30 || $numeroAtual == 40 ){
+            $i++;
+            continue;
+        }
+        echo " Elemento: $numeroAtual <br> ";
+        $i++;
     }
-
 ?>
