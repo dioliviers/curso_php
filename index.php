b0e3373a4bc69228270a884a6ba1537759fd5763
dioliviers <dioliviers@gmail.com>
2023-10-07 19:10:58 -0300

    utilizando include para inserir elementos de outros arquivos

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php
-   $arr =[];
-
-   for ($i= 0; $i <= 10; $i++){
-    array_push($arr,$i); /* array_push, pega a variavel vazia e insere o valor do segundo paramentro na primeira variavel como array */
-   }
-   print_r($arr);/* exibe o array */
-  
+include "teste.php";
 ?>
+<p> Após o include </p>
---
+++ teste.php
@@ -0,0 +1,5 @@
+<?php
+    $c = 5;
+?>
+<p> Após o include </p>
+<p> Imprimindo c <?php echo $c; ?> </p>
