cb5bc2ee3d2fea8e4fc4fdb899a8421ac804f86d
dioliviers <dioliviers@gmail.com>
2023-10-08 20:13:23 -0300

    exemplo de interpolação de variável php dentro de uma estrutura HTML

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
-    echo ("Este é o inicio do texto");
-    include "teste.php";
-    include "teste.php"; /* com include posso repetir a chamada do arquivo.php  */
-    require_once "require.php"; /* com require_once a chamada só pode ser feita uma vez , se chamar duas vezes ele não é apresentado */
-    require_once "require.php";
+    $nome = "Diogo";
 ?>
-<p> Exibindo conteúdo de teste <?php echo $idade; ?> </p> <!-- consigo também chamar variável php -->
+<form action="">
+    <div>
+        <input type="text" value="<?php echo $nome; ?> ">
+    </div>
+    <div>
+        <input type="submit" value="Enviar">
+    </div>
+</form>
