f3b432728d15508e6cb2d16ae01d145eee22e8f4
dioliviers <dioliviers@gmail.com>
2023-10-08 21:16:02 -0300

    coletando mais informações do backend e adicionando na estrutura html

---
+++ backend.php
@@ -0,0 +1,4 @@
+<?php
+    $nome = "Diogo";
+    $produtos = ["carro","Avião","Lancha"];
+?>
--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
-    $nome = "Diogo";
+    include_once "backend.php";
 ?>
-<form action="">
-    <div>
-        <input type="text" value="<?php echo $nome; ?> ">
-    </div>
-    <div>
-        <input type="submit" value="Enviar">
-    </div>
-</form>
+
+<h1> Seja bem-vindo ao nosso site </h1>
+<p><?= $nome; ?> veja as nossas ofertas </p>
+
+<h2> Confira nossos produtos </h2>
+
+<ul>
+    <?php foreach ( $produtos as $produtos ) : ?>
+        <li> <?= $produtos ?></li>
+    <?php endforeach; ?>
+</ul>
+
+
+    
