03b0beca76aeeb1ec8fefe339ff208eed5f08923
dioliviers <dioliviers@gmail.com>
2023-10-12 19:19:07 -0300

    exercicio 33 - exercicio com funções e parametro

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
-/* exercicio 31 
-Faça um array de string e inclua a função implode para inserir novos 
-elementos em cada string do array */
-    $array = ['Diogo','Pedro', 'Lubia'];
-    echo implode(' , ',$array);
-    /* aqui incluimos virgula para cada string, podemos inserir qualquer
-    coisa utilizando a função implode */
+/* exercicio 33 */
+    $nome = "Diogo";
+    $idade = 29;
+  function dados ($nome,$idade){
+    echo "Olá eu sou o $nome e tenho $idade ";
+  };
+  dados($nome,$idade);
+
 ?>
 
 
