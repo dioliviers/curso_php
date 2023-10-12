824f82fbba1cd134ace597577a11b9d588d81056
dioliviers <dioliviers@gmail.com>
2023-10-12 20:48:49 -0300

    exercicio para calculo de raiz quadrada e calculo ao quadrado

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
 <?php
-/* exercicio 34 verificar se o numero é par ou impar */
-    function verificaNumero($num1){ 
-        if( $num1 %2 == 0 ){
-            echo "O número é par";
-        }else{
-            echo "O número é impar";
-        }
-    }
-    verificaNumero(2);
+    /* exercicio  35 */
 
+    /* calculo da raiz quadrada  */
+    function calcNum($num){
+        $res = "A raiz quadrada de $num é " . sqrt($num) . " OK ? </br>";
+        return $res;
+    }
+    echo calcNum(10);
+    ?> </br>
+     <?php
+    /* Calculo do valor ao quadrado */
+    function calcQuadrado($num1){
+        $res =  "O número digitado foi $num1 " . "A raiz quadrada de $num1 é : ". pow($num1,$num1);
+        return $res;
+    }
+    echo calcQuadrado(2);
 ?>
 
 
